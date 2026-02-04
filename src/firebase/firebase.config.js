b5162c973bb257489afd26b4840bb2e362468746
mahia <mahiarahman2003@gmail.com>
2026-02-04 13:43:45 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_t2HIvk1ipKw5E0a9W6ejPDmv01S1gz4",
+  authDomain: "b12-a11-mahia.firebaseapp.com",
+  projectId: "b12-a11-mahia",
+  storageBucket: "b12-a11-mahia.firebasestorage.app",
+  messagingSenderId: "22706388838",
+  appId: "1:22706388838:web:69a38955d385d08aded2a7"
+};
+
+
+export const app = initializeApp(firebaseConfig)
